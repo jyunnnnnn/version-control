613facc27bfff2b3d81c71df622143f263f3d47c
jyun <andrew98311@gmail.com>
2023-10-25 19:13:53 +0800

    new print

--- myCode.py
+++ myCode.py
@@ -1,4 +1,3 @@
-
 #1.
 n1=int(input("請輸入數字一: "))
 n2=int(input("請輸入數字二: "))
@@ -8,27 +7,7 @@ if op== "+":
     print("答案為:",n1+n2)
 elif op== "-":
     print("答案為:",n1-n2)
-elif op== "*":
-    print("答案為:",n1*n2)
-elif op== "/":
-    if n2==0:
-        print("除數不得為0")
-    else:    
-        print("答案為:",n1/n2)
-elif op== "%":
-    if n2==0:
-        print("除數不得為0")
-    else:    
-        print("答案為:",n1%n2)
-elif op== "//":
-    if n2==0:
-        print("除數不得為0")
-    else:
-        print("答案為:",n1//n2)
-elif op== "^":
-    print("答案為:",n1**n2)
-else:
-    print("不支援的運算")
+print("new branch")
 #2.
 for i in range(1, 10):
     for j in range(1, 10):
