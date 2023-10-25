99068e031de110695ddbd2c9839d46f5f4459462
jyun <andrew98311@gmail.com>
2023-10-25 19:16:53 +0800

    new master version

--- myCode.py
+++ myCode.py
@@ -3,10 +3,6 @@ n1=int(input("請輸入數字一: "))
 n2=int(input("請輸入數字二: "))
 print("請輸入運算: +, -, *, /, %, //, ^")
 op=input()
-if op== "+":
-    print("答案為:",n1+n2)
-elif op== "-":
-    print("答案為:",n1-n2)
 print("new branch")
 #2.
 for i in range(1, 10):
